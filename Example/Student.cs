0983e9fd847c35434e8462843b5c6250c2c42fa7
justi <justi@QUANGDUY>
2022-01-26 18:50:51 +0700

    Add project files.

---
+++ Example/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public String StudentName { get; set; }
+        public int Age { get; set; }
+    }
+}
