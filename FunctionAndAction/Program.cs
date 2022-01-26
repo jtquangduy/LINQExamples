41882883a4a54eedf8aa82affed6ebe37006d94c
Quang Duy <justinquangduy@gmail.com>
2022-01-26 23:36:11 +0700

    FunctionAndAction

---
+++ FunctionAndAction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FunctionAndAction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int> myAction = new Action<int>(DoSomething);
+            myAction(123);
+
+            Func<int, double> myFunc = new Func<int, double>(CalculateSomething);
+            Console.WriteLine(myFunc(5));
+            Console.ReadKey();
+
+        }
+
+        static void DoSomething(int i)
+        {
+            Console.WriteLine(i);
+        }
+
+        static double CalculateSomething(int i)
+        {
+            return (double)i / 2;
+        }
+    }
+}
