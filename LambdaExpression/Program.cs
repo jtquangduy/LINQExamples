097a4c098aa49a3ccf3672df65da5a22d1b740bc
Quang Duy <justinquangduy@gmail.com>
2022-01-26 23:35:54 +0700

    LambdaExpression

---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee() {Id=1,Name="Alex"},
+                new Employee() {Id=2,Name="Barry"}
+            };
+
+            //foreach(var employee in developers.Where(NameStartWithA))
+            //{
+            //    Console.WriteLine(employee.Name);
+            //}
+
+            foreach (var employee in developers.Where(e => e.Name.StartsWith("A")))
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            Console.ReadKey();
+        }
+
+        //private static bool NameStartWithA(Employee employee)
+        //{
+        //    return employee.Name.StartsWith("A");
+        //}
+    }
+}
