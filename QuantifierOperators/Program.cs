28a9e41cc0a8413f6486b02d61bc7eaff4c6e491
Quang Duy <justinquangduy@gmail.com>
2022-01-27 21:19:35 +0700

    QuantifierOperators

---
+++ QuantifierOperators/Program.cs
@@ -0,0 +1,44 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace QuantifierOperators
+{
+    class StudentComparer : IEqualityComparer<Student>
+    {
+        public bool Equals(Student x, Student y)
+        {
+            if (x.StudentID == y.StudentID && x.StudentName.ToLower() == y.StudentName.ToLower())
+                return true;
+            return false;
+        }
+
+        public int GetHashCode(Student obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentID = 2, StudentName = "Steve",  Age = 15 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 19 }
+            };
+
+            //bool areAllStudentsTeenager = studentList.All(s => s.Age > 12 && s.Age < 20);
+
+            bool areAllStudentsTeenager = studentList.Any(s => s.Age > 12 && s.Age < 20);
+
+            Console.WriteLine(areAllStudentsTeenager);
+            Console.ReadKey();
+
+        }
+    }
+}
